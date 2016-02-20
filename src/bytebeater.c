a1f8e4cce5c9959fb2ab7216432cb65a15151849
k0f <krystof.pesek@gmail.com>
2016-02-20 22:13:21 +0100

    working window

--- src/bytebeater.c
+++ src/bytebeater.c
@@ -6,8 +6,12 @@
 
 #define WIDTH 640
 #define HEIGHT 480
-#define BPP 4
+#define BPP 8
 #define DEPTH 32
+#define sec 1000;
+
+
+float fps = 60;
 
 char buf[BUFSIZE];
 char *pIn, *pOut, *pEnd;
@@ -15,142 +19,98 @@ char full;
 int t;
 unsigned char running = TRUE;
 
-
-void buf_init(){
-  pIn = pOut = buf;
-  pEnd = &buf[BUFSIZE];
-  full = 0;
-}
-
-int buf_put(char c){
-  if(pIn == pOut && full)
-    return 0;
-
-  *pIn++ = c;
-  if(pIn >= pEnd)
-    pIn = buf;
-
-  if(pIn == pOut)
-    full = 1;
-
-  return 1;
-}
-
-int buf_get(char *pc){
-  if(pIn == pOut && !full)
-    return 0;
-
-  *pc = *pOut++;
-  if(pOut >= pEnd)
-    pOut = buf;
-
-  full = 0;
-  return 1;
-
-}
-void putpixel(SDL_Surface *surface, int x, int y, int pixel)
-{
-  int bpp = surface->format->BytesPerPixel;
-  /* Here p is the address to the pixel we want to set */
-  Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
-
-  switch(bpp) {
-    case 1:
-      *p = pixel;
-      break;
-
-    case 2:
-      *(Uint16 *)p = pixel;
-      break;
-
-    case 3:
-      if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-        p[0] = (pixel >> 16) & 0xff;
-        p[1] = (pixel >> 8) & 0xff;
-        p[2] = pixel & 0xff;
-      } else {
-        p[0] = pixel & 0xff;
-        p[1] = (pixel >> 8) & 0xff;
-        p[2] = (pixel >> 16) & 0xff;
-      }
-      break;
-
-    case 4:
-      *(Uint32 *)p = pixel;
-      break;
-  }
-}
+typedef struct{
+  Uint8 r;
+  Uint8 g;
+  Uint8 b;
+  Uint8 alpha;
+} pixel;
 
 
-void DrawScreen(SDL_Surface* screen, int h)
-{ 
-  int x, y, ytimesw;
 
-  //buf_put(t % BUFSIZE);
-  //t++;
-  //int pix = buf_get(t%BUFSIZE);
-
-
-  if(SDL_MUSTLOCK(screen)) 
-  {
-    if(SDL_LockSurface(screen) < 0) return;
-  }
-
-  for(y = 0; y < screen->h; y++ ) 
-  {
-    ytimesw = y*screen->pitch/BPP;
-    for( x = 0; x < screen->w; x++ ) 
-    {
-      putpixel(screen, x, ytimesw, h);
-    }
-  }
-
-  if(SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
-
-  SDL_Flip(screen); 
-}
 
 
 int main(int argc,char *argv[]){
 
-  buf_init();
+
+  /* Initialize defaults, Video and Audio */
+
+  if( SDL_Init(SDL_INIT_VIDEO)==(-1) ) { 
+
+    printf("Could not initialize SDL: %s.\n", SDL_GetError());
+
+    exit(-1);
+
+  }
+
+
+
+  /* Set up the video */
 
   SDL_Surface *screen;
-  SDL_Event event;
-
-  int keypress = 0;
-  int h = 0;
 
 
-  if(SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
+  screen = SDL_SetVideoMode( WIDTH, HEIGHT, BPP, SDL_HWSURFACE );
 
-  if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
-  {
-    SDL_Quit();
-    return 1;
+  if( screen == NULL ) {
+
+    fprintf(stderr, "Couldn't set %ix%ix%i video mode: %s\n", WIDTH,HEIGHT,BPP, SDL_GetError());
+
+    exit(1);
+
   }
 
-  while(TRUE) 
-  {
-    DrawScreen(screen,h++);
-    while(SDL_PollEvent(&event)) 
-    {      
-      switch (event.type) 
-      {
-        case SDL_QUIT:
-          keypress = 1;
-          break;
-        case SDL_KEYDOWN:
-          keypress = 1;
-          break;
+
+  //SDL_ShowCursor( SDL_DISABLE ); // The cursor is ugly :)
+
+
+
+  /* Set up the SDL_TTF */
+
+  TTF_Init();
+  TTF_Font *font = TTF_OpenFont( "assets/SempliceRegular.ttf", 8 );
+
+  SDL_Color clrFg = {255,255,255,0};  // Blue ("Fg" is foreground)
+  SDL_Surface *sText = TTF_RenderText_Solid( font, "Some random text", clrFg );
+  SDL_Rect rcDest = {0,0,0,0};
+  SDL_BlitSurface( sText,NULL, screen,&rcDest );
+  SDL_FreeSurface( sText );
+
+  float minframetime = (1000.0 / fps);
+  int running = 1;
+  SDL_Event event;
+  Uint32 frametime;
+
+  while(running==1){
+    frametime = SDL_GetTicks (); 
+
+    while (SDL_PollEvent (&event) != 0){
+      switch(event.type){
+        case SDL_KEYDOWN: if (event.key.keysym.sym == SDLK_ESCAPE)
+                            running = 0;
+                          break;
       }
     }
+
+
+    if (SDL_GetTicks () - frametime < minframetime)
+      SDL_Delay (minframetime - (SDL_GetTicks () - frametime));
+      
+    
+    SDL_Flip (screen);
+
   }
 
-  SDL_Quit();
 
+  TTF_CloseFont( font );
 
+  atexit(TTF_Quit);
+
+  SDL_Flip (screen);
+  SDL_FreeSurface (screen);
+  SDL_Delay (200);
 
 
   return 0;
+
 }
